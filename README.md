e73bd7ee14938cfa42feeeb22ddea2a3ca2586ba
Mister Magson <magson@gmail.com>
2018-01-03 09:11:23 -0300

    Correct indentation and formatting

--- README.md
+++ README.md
@@ -8,13 +8,10 @@ API Key
 -------
 You will need an API key to The Movie Database to access the API.  To obtain a key, follow these steps:
 
-1) Register for and verify an account_.
-2) `Log into`_ your account.
+1) Register for and verify an account at https://www.themoviedb.org/account/signup
+2) `Log into`_ your account at https://www.themoviedb.org/login
 3) Select the API section on left side of your account page.
 4) Click on the link to generate a new API key and follow the instructions.
-
-.. _account: https://www.themoviedb.org/account/signup
-.. _Log into: https://www.themoviedb.org/login
 
 ## Usage
 
@@ -27,4 +24,3 @@ Run tmdb_entertainment_center.py
 ```bash
 python tmdb_entertainment_center.py
 ```
-
--- media.py
+++ media.py
@@ -1,14 +1,29 @@
 import webbrowser
-class Movie():
-    """Class stores the structure of the movies"""
-    
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
 
-    def show_trailer(self):
-        """It shows the trailer of the movie on Youtube"""
-        webbrowser.open(self.trailer_youtube_url)
+
+class Movie():
+
+    """Class stores the structure of the movies
+
+    Attributes:
+       movie_title (string): The title of the movie
+
+       movie_storyline (string): a short description of the movie
+
+       poster_image (string): a link to a image with the poster
+
+       trailer_youtube(string): a link to youtube with the trailer
+    """
+
+
+def __init__(self, movie_title, movie_storyline, poster_image,
+             trailer_youtube):
+    self.title = movie_title
+    self.storyline = movie_storyline
+    self.poster_image_url = poster_image
+    self.trailer_youtube_url = trailer_youtube
+
+
+def show_trailer(self):
+    """It shows the trailer of the movie on Youtube"""
+    webbrowser.open(self.trailer_youtube_url)
